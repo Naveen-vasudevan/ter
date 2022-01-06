4204578f0615f18f28e77941e9c2cd6f062eef52
root <root@ip-172-31-32-47.ap-south-1.compute.internal>
2022-01-06 13:22:49 +0000

    aiyyos

--- terraform-manifest/ec2.tf
+++ terraform-manifest/ec2.tf
@@ -7,30 +7,20 @@ provider "aws" {
 
 
 module "ec2_instance" {
-source = "terraform-aws-modules/ec2-instance/aws"
-version = "~> 3.0"
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
 
+  name = "single-instance"
 
+  ami                    = "ami-052cef05d01020f1d"
+  instance_type          = "t2.micro"
+ # key_name               = "user1"
+ # monitoring             = true
+ # vpc_security_group_ids = ["sg-12345678"]
+ # subnet_id              = "subnet-eddcdzz4"
 
-for_each = toset(["001", "002", "003"])
-
-
-
-name = "VALPRO${each.key}"
-
-
-
-ami = "ami-052cef05d01020f1d"
-instance_type = "t2.micro"
-#key_name = "user1"
-#monitoring = true
-#vpc_security_group_ids = ["sg-12345678"]
-#subnet_id = "subnet-eddcdzz4"
-
-
-
-tags = {
-Terraform = "true"
-Environment = "dev"
-}
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
 }
